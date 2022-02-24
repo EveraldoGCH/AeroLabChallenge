943eb442c118e0a73f0a1c76c0323babf3400e2b
Everaldo Guevara <everaldo.guevara@DH.digitalhouse.com>
2022-02-24 19:58:17 -0300

    WAVES COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Bloque1 from './assets/components/Bloque1';
 import NavBar from './assets/components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Bloque1/>
     </div>
   );
 }
---
+++ src/assets/components/Bloque1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./css/Bloque1.css";
+import Waves from "./Waves"
+
+
+export default function Bloque1() {
+  return (
+    <div className='Bloque1'>
+    <Waves/>
+    <Waves/>
+    </div>
+  );
+}
