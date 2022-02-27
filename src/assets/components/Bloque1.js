3a321ef407207702b2ba4dbf3bb25e7f3844bfa3
Everaldo Guevara <everaldo.guevara@DH.digitalhouse.com>
2022-02-26 21:30:11 -0300

    mobile version half done

--- src/assets/components/Bloque1.js
+++ src/assets/components/Bloque1.js
@@ -6,6 +6,17 @@ import Waves from "./Waves"
 export default function Bloque1() {
   return (
     <div className='Bloque1'>
+        <div className='textContainer'>
+            <p className='explore'>EXPLORE THE</p>
+            <h1 className='tech'>TECH</h1>
+            <h1 className='zone'>ZONE</h1>
+            <p className='hereYou'>Here you’ll be able to redeem all of your hard-earned Aeropoints and exchange them for cool tech.</p>
+            <div className='parentViewAllProducts'>
+                <button href='#productos' className='viewAllProductsBtn'>VIEW ALL PRODUCTS</button>
+            </div>
+        </div>
+    <Waves/>
+    <Waves/>
     <Waves/>
     <Waves/>
     </div>
---
+++ src/assets/components/Ilustracion.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import imagen1 from 
+
+
+export default function Ilustracion() {
+  return (
+    <div>
+    </div>
+  );
+}
